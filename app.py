fac41d2d9b85fd2aa641058c89e9c1febbe6fb63
THE-MUZAMIL-MEER <127821271+Muzamilmeer@users.noreply.github.com>
2025-07-15 13:57:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+import os
 
 app = Flask(__name__)
 CORS(app)
@@ -11,4 +12,5 @@ def submit():
     return jsonify({"message": "Feedback received successfully!"}), 200
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
